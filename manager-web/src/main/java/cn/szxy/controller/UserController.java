9e01860101d304e9d3cdd49f4aca0d59281bccaf
zweros <414356480@qq.com>
2019-05-28 18:07:17 +0800

    使用 RESTful Web Service 测试工具的测试代码

--- manager-web/src/main/java/cn/szxy/controller/UserController.java
+++ manager-web/src/main/java/cn/szxy/controller/UserController.java
@@ -4,7 +4,10 @@ import cn.szxy.pojo.Users;
 import cn.szxy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Auther:wzer
@@ -24,4 +27,43 @@ public class UserController {
         this.userService.addUser(user);
         return "ok";
     }
+
+    // 测试 工具 Tools -> HTTP Client ->  Test RESTful Client Web Service
+    /**
+     *  doGet 方法
+     */
+    @RequestMapping(value = "/doGet/{id}" , method=RequestMethod.GET)
+    public String doGet(@PathVariable  String id){
+        System.out.println("ok....."+id);
+        return "ok";
+    }
+
+    /**
+     *  doPost 方法
+     */
+    @RequestMapping(value="/doPost",method=RequestMethod.POST)
+    public String doPost(Users user){
+        System.out.println("ok...."+user);
+        return "ok";
+    }
+
+    /**
+     *  doPut 方法，用于更新某些信息
+     */
+    @RequestMapping(value="/doPut/{id}",method=RequestMethod.PUT)
+    public String doPut(@PathVariable String id){
+        System.out.println("ok..... put: "+id);
+        return "ok";
+    }
+
+    /**
+     *  doDelete 方法，用于删除某些信息
+     */
+    @RequestMapping(value="/doDelete/{id}",method=RequestMethod.DELETE)
+    public String doDelete(@PathVariable String id){
+        System.out.println("ok..... delete:"+id);
+        return "ok";
+    }
 }
+
+
